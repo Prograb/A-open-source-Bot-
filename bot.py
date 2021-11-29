2aa0c2f03343c491e60436e84743e1ccf2931a20
Mihaidev-cloud <69433258+Mihaidev-cloud@users.noreply.github.com>
2021-11-29 17:28:21 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix='YOUR PREFIX)
+                   
+                   
+@bot.command()
+  async def hello(ctx):
+        await ctx.send('Hello this is a bot with open source code")
+                       
+                       
+                       
+                   @bot.command()
+                       async def Ping(ctx):
+                       await ctx.send('Pong')
+                  
+                bot.run('your token bot')
